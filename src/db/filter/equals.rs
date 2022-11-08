43b23e368522ace5d0ca9509acca0fe3250f8b78
Ren Shao <ren@shao.io>
2022-11-08 11:19:00 +1100

    Parse various date format
    Supports following date filters:
    - WHERE date = 10 (integer is the month number)
    - WHERE date = '2022-10-12'
    - WHERE date = '2019-08'

--- src/db/filter/equals.rs
+++ src/db/filter/equals.rs
@@ -1,5 +1,9 @@
 use std::collections::HashSet;
-use chrono::{Datelike, NaiveDate, Utc};
+use std::str::Split;
+use std::ops::{Add, Range};
+use chrono::{Datelike, Duration, NaiveDate, Utc};
+use log::{warn};
+use regex::Regex;
 use sqlparser::ast::{Expr, Value};
 use sqlparser::ast::Expr::Identifier;
 use crate::db::Database;
@@ -21,30 +25,18 @@ pub(crate) fn handle_equals(left: Expr, right: Expr, database: &Database, transa
                 },
 
                 "date" => {
-                    if let Expr::Value(Value::Number(num_str, _)) = right {
-                        let date = num_str.parse::<u32>().unwrap();
-                        // if month
-                        if date >= 1 && date <= 12 {
-                            let month = date;
-                            let today = Utc::now().naive_utc().date();
-                            let mut year = today.year();
-                            if month >= today.month() {
-                                year -= 1;
-                            }
-
-                            let first_day = NaiveDate::from_ymd(year, month, 1);
-                            let next_month = if month == 12 { 1 } else { month + 1 };
-                            let next_month_year = if month == 12 { year + 1 } else { year };
-                            let first_day_next_month = NaiveDate::from_ymd(next_month_year, next_month, 1);
-
+                    match parse_date_range(&right) {
+                        Ok(date_range) => {
                             let mut trans_in_date_range = HashSet::<u32>::new();
-                            for (_, trans_ids) in database.date_index.range(first_day..first_day_next_month) {
+                            for (_, trans_ids) in database.date_index.range(date_range) {
                                 for id in trans_ids {
                                     trans_in_date_range.insert(*id);
                                 }
                             }
-
                             return transactions.intersection(&trans_in_date_range).cloned().collect();
+                        },
+                        Err(()) => {
+                            warn!("Unable to parse date {}", right.to_string());
                         }
                     }
                 },
@@ -56,4 +48,49 @@ pub(crate) fn handle_equals(left: Expr, right: Expr, database: &Database, transa
     }
 
     HashSet::new()
+}
+
+fn parse_date_range(date_expr: &Expr) -> Result<Range<NaiveDate>, ()> {
+    if let Expr::Value(Value::Number(num_str, _)) = date_expr {
+        let date = num_str.parse::<u32>().unwrap();
+        // if month
+        if date >= 1 && date <= 12 {
+            let month = date;
+            let today = Utc::now().naive_utc().date();
+            let mut year = today.year();
+            if month >= today.month() {
+                year -= 1;
+            }
+
+            let first_day = NaiveDate::from_ymd(year, month, 1);
+            let next_month = if month == 12 { 1 } else { month + 1 };
+            let next_month_year = if month == 12 { year + 1 } else { year };
+            let first_day_next_month = NaiveDate::from_ymd(next_month_year, next_month, 1);
+
+            return Ok(first_day..first_day_next_month);
+        }
+    } else if let Expr::Value(Value::SingleQuotedString(string)) = date_expr {
+        // Handle date format '2022-09-03'
+        let yyyy_mm_dd = Regex::new(r"^\d{4}-\d{2}-\d{2}$").unwrap();
+        if yyyy_mm_dd.is_match(&string) {
+            let date = NaiveDate::parse_from_str(string.as_str(), "%Y-%m-%d").unwrap();
+            return Ok(date..date.add(Duration::days(1)));
+        }
+
+        let yyyy_mm = Regex::new(r"^\d{4}-\d{2}$").unwrap();
+        if yyyy_mm.is_match(&string) {
+            let splitted: Vec<&str> = string.split('-').collect();
+            let year = splitted[0].to_string().parse::<i32>().unwrap();
+            let month = splitted[1].to_string().parse::<u32>().unwrap();
+
+            let first_day = NaiveDate::from_ymd(year, month, 1);
+            let next_month = if month == 12 { 1 } else { month + 1 };
+            let next_month_year = if month == 12 { year + 1 } else { year };
+            let first_day_next_month = NaiveDate::from_ymd(next_month_year, next_month, 1);
+
+            return Ok(first_day..first_day_next_month);
+        }
+    }
+
+    Err(())
 }
