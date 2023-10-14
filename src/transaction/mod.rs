14c9a96371bb338379d595885e14d4ab72293da1
Ren Shao <ren@shao.io>
2023-10-15 09:52:27 +1100

    Added a transaction hash function
    This will be used to identify existing transactions and detecting
    duplicate imports.

--- src/transaction/mod.rs
+++ src/transaction/mod.rs
@@ -1,3 +1,5 @@
+use std::collections::hash_map::DefaultHasher;
+use std::hash::Hasher;
 use chrono::NaiveDateTime;
 use serde::Serializer;
 
@@ -33,8 +35,49 @@ impl Transaction {
     }
 }
 
+/// A hash function based on a transaction's content.
+/// We use amount's absolute value because sometimes we need to deal with inverted amount,
+/// e.g. in the statement we have $96 but the same transaction already imported had -$96,
+/// if both transactions have the same date and description we want the hash to be the same.
+pub(crate) fn transaction_hash(datetime: NaiveDateTime, description: &str, amount: f32) -> u64 {
+    let mut hasher = DefaultHasher::new();
+    hasher.write_i64(datetime.timestamp());
+    hasher.write(description.as_bytes());
+    hasher.write(&amount.abs().to_le_bytes());
+
+    hasher.finish()
+}
+
 /// Join all tags by a bar |
 fn serialise_labels<S>(tags: &[String], serializer: S) -> Result<S::Ok, S::Error>
     where S: Serializer {
     serializer.collect_str(tags.join("|").as_str())
+}
+
+#[cfg(test)]
+mod tests {
+    use std::str::FromStr;
+    use chrono::NaiveDateTime;
+    use crate::transaction::transaction_hash;
+
+    #[test]
+    fn test() {
+        let datetime1 = NaiveDateTime::from_str("2023-10-11T11:15:34").unwrap();
+        let datetime2 = NaiveDateTime::from_str("2023-10-11T11:15:35").unwrap();
+        assert_eq!(
+            transaction_hash(datetime1, "Buy milk", 32.0),
+            transaction_hash(datetime1, "Buy milk", 32.0)
+        );
+
+        // Verify inverted amount results same hash
+        assert_eq!(
+            transaction_hash(datetime1, "Buy milk", 32.56),
+            transaction_hash(datetime1, "Buy milk", -32.56)
+        );
+
+        assert_ne!(
+            transaction_hash(datetime1, "Buy milk", 32.0),
+            transaction_hash(datetime2, "Buy milk", 32.0)
+        );
+    }
 }
